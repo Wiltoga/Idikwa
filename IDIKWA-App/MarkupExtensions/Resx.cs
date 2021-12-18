1a1e11396bf1f5ebc4d4a611995aa6c202875a5a
Nathan HANEN <hanen.nathan@gmail.com>
2021-12-18 14:05:46 +0100

    added localization support

---
+++ IDIKWA-App/MarkupExtensions/Resx.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Markup.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDIKWA_App
+{
+    public class Resx : MarkupExtension
+    {
+        public Resx()
+        {
+            Key = null;
+        }
+
+        public Resx(string key) => Key = key;
+
+        public string? Key { get; set; }
+
+        public override object? ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Key is null)
+                throw new InvalidOperationException("The resource key can not be null.");
+            return locales.AppResources.ResourceManager.GetObject(Key);
+        }
+    }
+}
