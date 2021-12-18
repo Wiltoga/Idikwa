1455113d6e76f346b0e928da213dd4377b613341
Nathan HANEN <hanen.nathan@gmail.com>
2021-12-18 12:17:42 +0100

    setup project files

---
+++ IDIKWA-App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDIKWA_App
+{
+    public class MainWindowViewModel : ReactiveObject
+    {
+        public MainWindowViewModel()
+        {
+        }
+    }
+}
