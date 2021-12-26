e10282836d68c4b08a6696daf76a6e14a49e1174
Nathan HANEN <hanen.nathan@gmail.com>
2021-12-26 10:09:25 +0100

    added factory

---
+++ IDIKWA-App/Models/IFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDIKWA_App
+{
+    public interface IFactory
+    {
+        public Task<string> SaveAsync(Record record);
+    }
+}
---
+++ IDIKWA-App/Models/LocalFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDIKWA_App
+{
+    public class LocalFactory : IFactory
+    {
+        public async Task<string> SaveAsync(Record record)
+        {
+            return await Task.FromResult("");
+        }
+    }
+}
