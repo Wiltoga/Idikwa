3e446549dafb80ec80f33bf238fb47f97cfd739d
Nathan HANEN <hanen.nathan@gmail.com>
2021-12-29 12:43:23 +0100

    added Recorder to IWaveProvider converter

---
+++ IDIKWA-App/Utils/RecorderWaveProvider.cs
@@ -0,0 +1,54 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IDIKWA_App
+{
+    public class RecorderWaveProvider : IWaveProvider
+    {
+        private WasapiCapture capture;
+
+        private Pipe pipe;
+
+        public RecorderWaveProvider(MMDevice device)
+        {
+            if (device.DataFlow == DataFlow.Render)
+                capture = new WasapiLoopbackCapture(device);
+            else
+                capture = new WasapiCapture(device);
+            pipe = new Pipe();
+            capture.DataAvailable += (sender, e) =>
+            {
+                pipe.Write(e.Buffer, 0, e.BytesRecorded);
+            };
+            capture.RecordingStopped += (sender, e) =>
+            {
+                capture.Dispose();
+            };
+        }
+
+        public WaveFormat WaveFormat => capture.WaveFormat;
+
+        public int Read(byte[] buffer, int offset, int count)
+        {
+            return pipe.Read(buffer, offset, count);
+        }
+
+        public void StartRecording()
+        {
+            capture.StartRecording();
+        }
+
+        public void StopRecording()
+        {
+            capture.StopRecording();
+            pipe.Close();
+        }
+    }
+}
