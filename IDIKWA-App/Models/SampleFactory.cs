01ffea7ca03e8f1c706e7674569f4ff17e740ffb
Nathan HANEN <hanen.nathan@gmail.com>
2022-01-07 23:48:17 +0100

    thread safe temporary wave stream

---
+++ IDIKWA-App/Models/SampleFactory.cs
@@ -0,0 +1,59 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDIKWA_App
+{
+    public class SampleFactory
+    {
+        public SampleFactory()
+        {
+            Recorders = new List<RecorderWaveProvider>();
+            TemporaryBuffers = new List<(TemporaryWaveStream, Task)>();
+        }
+
+        private List<RecorderWaveProvider> Recorders { get; }
+        private List<(TemporaryWaveStream, Task)> TemporaryBuffers { get; }
+
+        public void StartRecord(IEnumerable<MMDevice> devices, WaveFormat format, TimeSpan bufferDuration)
+        {
+            if (Recorders.Any())
+                throw new InvalidOperationException("ALready recording");
+            Recorders.Clear();
+            TemporaryBuffers.Clear();
+            foreach (var device in devices)
+            {
+                var recorder = new RecorderWaveProvider(device);
+                var sampler = new MediaFoundationResampler(recorder, format);
+                var buffer = new TemporaryWaveStream(sampler, bufferDuration);
+                Recorders.Add(recorder);
+                TemporaryBuffers.Add((buffer, buffer.ListenAsync()));
+            }
+            foreach (var recorder in Recorders)
+            {
+                recorder.StartRecording();
+            }
+        }
+
+        public async Task<WaveStream[]> StopRecord()
+        {
+            foreach (var recorder in Recorders)
+            {
+                recorder.StopRecording();
+            }
+            foreach (var buffer in TemporaryBuffers)
+            {
+                await buffer.Item2;
+                buffer.Item1.Seek(0, System.IO.SeekOrigin.Begin);
+            }
+            var result = TemporaryBuffers.Select(buffer => buffer.Item1).ToArray();
+            Recorders.Clear();
+            TemporaryBuffers.Clear();
+            return result;
+        }
+    }
+}
---
+++ IDIKWA-App/Utils/TemporaryWaveStream.cs
@@ -0,0 +1,94 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IDIKWA_App
+{
+    public class TemporaryWaveStream : WaveStream, IWaveProvider
+    {
+        public TemporaryWaveStream(IWaveProvider provider, TimeSpan duration)
+        {
+            Mutex = new SemaphoreSlim(1);
+            Source = provider;
+            Stream = new TemporaryStream((int)(provider.WaveFormat.AverageBytesPerSecond * duration.TotalSeconds));
+        }
+
+        public override long Length => Stream.Length;
+
+        public override long Position
+        {
+            get
+            {
+                long result;
+                Mutex.Wait();
+                try
+                {
+                    result = Stream.Position;
+                }
+                finally
+                {
+                    Mutex.Release();
+                }
+                return result;
+            }
+            set
+            {
+                Mutex.Wait();
+                try
+                {
+                    Stream.Position = value;
+                }
+                finally
+                {
+                    Mutex.Release();
+                }
+            }
+        }
+
+        public override WaveFormat WaveFormat => Source.WaveFormat;
+        private SemaphoreSlim Mutex { get; }
+        private IWaveProvider Source { get; }
+        private Stream Stream { get; }
+
+        public async Task ListenAsync()
+        {
+            var bytes = new byte[1024 * 16];
+            while (true)
+            {
+                int bytesRead = await Task.Run(() => Source.Read(bytes, 0, bytes.Length));
+                if (bytesRead == 0)
+                    break;
+                await Mutex.WaitAsync();
+                try
+                {
+                    await Stream.WriteAsync(bytes, 0, bytesRead);
+                }
+                finally
+                {
+                    Mutex.Release();
+                }
+            }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            int readBytes;
+            Mutex.Wait();
+            try
+            {
+                readBytes = Stream.Read(buffer, offset, count);
+            }
+            finally
+            {
+                Mutex.Release();
+            }
+
+            return readBytes;
+        }
+    }
+}
